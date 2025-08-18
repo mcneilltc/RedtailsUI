598885b2b5e8e0697075845f8565cd8c908a5230
Tiquila McNeill <tiquilamcneill@gmail.com>
2025-08-17 21:36:04 -0400

    icon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,20 @@ const alkatra = localFont({
 export const metadata: Metadata = {
   title: "Redtails",
   description: "Book your outdoor adventure",
+  openGraph: {
+    images: [
+      {
+        url: "/images/logos/logo2.png",
+        width: 1200,
+        height: 630,
+        alt: "Red Tails Outdoors Logo",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: ["/images/logos/logo2.png"],
+  },
 };
 
 export default function RootLayout({
@@ -24,17 +38,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <body>
-      <ThemeProvider>
-        <div className="navigation-wrapper">
-          <Navigation />
-        </div>
-        <main className="content-wrapper">
-          {children}
-          <ContactUs />
-        </main>
-      </ThemeProvider>
-    </body>
-  </html>
+      <head>
+        <link rel="icon" href="/images/logos/logo2.png" type="image/png" />
+        {/* Or use favicon.ico */}
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <title>Red Tails Outdoors</title>
+      </head>
+      <body>
+        <ThemeProvider>
+          <div className="navigation-wrapper">
+            <Navigation />
+          </div>
+          <main className="content-wrapper">
+            {children}
+            <ContactUs />
+          </main>
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
